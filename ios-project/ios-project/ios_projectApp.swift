fbbd22ceabdf71ccb0168e3c1295a96330ac6fe0
MufliMohideen <mufli.m@expernetic.com>
2026-06-10 18:59:18 +0530

    initial commit

---
+++ ios-project/ios-project/ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_projectApp.swift
+//  ios-project
+//
+//  Created by Mufli Mohideen on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-project/ios-projectTests/ios_projectTests.swift
@@ -0,0 +1,19 @@
+//
+//  ios_projectTests.swift
+//  ios-projectTests
+//
+//  Created by Mufli Mohideen on 2026-06-10.
+//
+
+import Testing
+@testable import ios_project
+
+struct ios_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
